0e63020411fbebb5cc31b4762c0c9af54a4d8cb4
nickcamp13 <nicholascampos013@gmail.com>
2024-04-21 00:58:59 -0700

    added right drawer + data fetching

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,29 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Grid } from "@mui/material";
+
 import Navbar from "./components/Navbar";
-import Posts from "./pages/Posts"
+import Home from "./pages/Home";
 import Create from "./pages/Create";
-import "./App.css";
+import RightDrawer from "./components/RightDrawer";
 
 function App() {
   return (
-    <>
-      <Navbar />
-        <Routes>
-          <Route path="/" element={<Posts />}/>
-          <Route path="/create-post" element={<Create />}/>
-        </Routes>
-    </>
+    <Router>
+      <Grid container>
+        <Grid item xs={12}>
+          <Navbar />
+        </Grid>
+        <Grid item md={10}>
+          <Routes>
+            <Route path="" element={<Home />} />
+            <Route path="create-post" element={<Create />} />
+          </Routes>
+        </Grid>
+        <Grid item md={2}>
+          <RightDrawer />
+        </Grid>
+      </Grid>
+    </Router>
   );
 }
 
