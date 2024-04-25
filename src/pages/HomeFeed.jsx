daea52cd43db85b5430c94e99cf7bf5ec0c5aea9
nickcamp13 <nicholascampos013@gmail.com>
2024-04-25 14:42:55 -0700

    added sort feature for time and upvote count

--- src/pages/HomeFeed.jsx
+++ src/pages/HomeFeed.jsx
@@ -8,28 +8,57 @@ const SPORTSDB_KEY = import.meta.env.VITE_SPORTSDB_ANON_KEY;
 const SPORTSDB_URL = `https://www.thesportsdb.com/api/v1/json/${SPORTSDB_KEY}`;
 
 const HomeFeed = () => {
-  const [feed, setFeed] = useState(null);
+  const [feed, setFeed] = useState([]);
+  const [sortedPosts, setSortedPosts] = useState(null);
+  const [orderBy, setOrderBy] = useState("time");
 
   useEffect(() => {
     const fetchFeed = async () => {
       const { data, error } = await supabase.from("posts").select();
       setFeed(data);
+      setSortedPosts(feed);
     };
 
     fetchFeed();
   }, []);
 
-  if (!feed) {
+  useEffect(() => {
+    sortPosts(orderBy);
+  }, [orderBy, feed]);
+
+  const sortPosts = (criteria) => {
+    const postsCopy = [...feed];
+    if (criteria === "time") {
+      postsCopy.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    } else if (criteria === "upvotes") {
+      postsCopy.sort((a, b) => b.upvotes - a.upvotes);
+    }
+    setSortedPosts(postsCopy);
+  };
+
+  const handleSortChange = (e) => {
+    setOrderBy(e.target.value);
+  };
+
+  if (feed.length === 0) {
     return <Box>Loading Feed...</Box>;
   }
   return (
     <Box>
-      {feed &&
-        feed.map((post) => (
-          <Link key={post.id} to={`post/${post.id}`}>
-            <FeedPost post={post} />
-          </Link>
-        ))}
+      <Box>
+        <select value={orderBy} onChange={handleSortChange}>
+          <option value="time">Sort by Time</option>
+          <option value="upvotes">Sort by Upvotes</option>
+        </select>
+      </Box>
+      <Box>
+        {feed &&
+          sortedPosts.map((post) => (
+            <Link key={post.id} to={`post/${post.id}`}>
+              <FeedPost post={post} />
+            </Link>
+          ))}
+      </Box>
     </Box>
   );
 };
